052b752a509e9ae4819ccbb8b905232534669ff1
SALAUDDIN <mdsalauddinrony29@gmail.com>
2026-08-01 20:42:37 +0600

    this is my firs python code

---
+++ comment.py
@@ -0,0 +1,10 @@
+#this is single line comments.
+"""this is multi line comment use.
+but actually it is a multiline string."""
+
+
+a = 20
+b= 50
+c = a+b
+
+print(f"this is {a} and {b} and the sum is {c}")
