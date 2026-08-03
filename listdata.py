6dbdd76a7d5d3bfa08b1f5f3bfd220ca379ab520
Md Salauddin Rony <mdsalauddinrony29@gmail.com>
2026-08-03 10:57:00 +0600

    complete the list operation

--- listdata.py
+++ listdata.py
@@ -4,3 +4,42 @@ for x in thisList:
 
 for y in range(len(thisList)):
     print(thisList[y])
+
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = [x for x in fruits if "a" in x]
+newList2 = fruits
+print(newList2)
+print(newlist)
+
+thislist = [100, 50, 65, 82, 23]
+thislist.sort()
+print(thislist)
+
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort(reverse = True)
+print(thislist)
+
+def myfunc(n):
+    return abs(n-50)
+thisList=[100,50,65,82,23]
+thisList.sort(key = myfunc)
+print(thisList)
+
+thisList=[100,50,65,82,23]
+myList=thisList.copy()
+print(myList)
+
+list1 = ["a", "b", "c"]
+list2 = [1, 2, 3]
+
+list3 = list1 + list2
+print(list3)
+
+list1.extend(list2)
+print(list1)
+
+fruits = ["apple", "cherry", "banana", "cherry"]
+x = fruits.count("cherry")
+print(x)
+x = fruits.index("apple")
+print(x)
